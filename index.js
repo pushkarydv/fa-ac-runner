02d51c591530ac0452cae3a1c7247720ca47a6c6
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2025-01-08 22:16:11 +0530

    sync

---
+++ index.js
@@ -0,0 +1,80 @@
+import gplay from 'google-play-scraper';
+
+const appsMap = new Map();
+const detailedAppsMap = new Map();
+
+const getAllCategoriesAndCollections = () => {
+  const categories = Object.keys(gplay.category);
+  const collections = Object.keys(gplay.collection);
+  console.log(
+    `${categories.length} categories and ${collections.length} collections each`
+  );
+
+  return { categories, collections };
+};
+
+const addAppsInPair = async ({ category, collection }) => {
+  try {
+    const apps = await gplay.list({
+      category: gplay.category[category],
+      collection: gplay.collection[collection],
+      num: 200,
+      country: 'in',
+    });
+
+    console.log(
+      `Fetched ${apps.length} apps for category: ${category}, collection: ${collection}`
+    );
+
+    apps.forEach((app) => {
+      if (!appsMap.has(app.appId)) {
+        appsMap.set(app.appId, app);
+      }
+    });
+
+    console.log(`Total unique apps now: ${appsMap.size}`);
+  } catch (error) {
+    console.error(
+      `Error fetching apps for category: ${category}, collection: ${collection}`,
+      error.message
+    );
+  }
+};
+
+
+
+
+  const fetchAppDetailed = async (appId) => {
+    try {
+      const app = await gplay.app({ appId });
+        detailedAppsMap.set(appId, app);
+        console.log(`detailed app size ${detailedAppsMap.size}`);
+        
+    } catch (error) {
+      console.error(`Error fetching app details for appId: ${appId}`, error.message);
+    }
+  };
+
+(async () => {
+  const { categories, collections } = getAllCategoriesAndCollections();
+  await Promise.all(
+    categories.flatMap((category) =>
+      collections.map((collection) => addAppsInPair({ category, collection }))
+    )
+  );
+
+
+  // run detail fetch
+    const appIds = Array.from(appsMap.keys());
+    console.log(`Total apps to fetch details: ${appIds.length}`);
+    for (const appId of appIds) {
+        await fetchAppDetailed(appId);
+    }
+
+
+  console.log(`Total unique apps: ${appsMap.size}`);
+    console.log(`Total unique detailed apps: ${detailedAppsMap.size}`);
+})();
+
+
+// 19733 apps / seconds
